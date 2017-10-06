b63115e22483b269224ab26acb932c5970977fec
mars code <juxxymars@gmail.com>
2017-10-06 10:05:24 +0300

    config updated

--- application/controllers/Lsm.php
+++ application/controllers/Lsm.php
@@ -7,10 +7,12 @@ class Lsm extends CI_Controller {
 	public function __construct()
 	{
 		parent:: __construct();
+		//$this->load->library('database');
 		$this->load->helper('url');
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 		$this->load->model('Lsm_model');
+
 
 	}
 
